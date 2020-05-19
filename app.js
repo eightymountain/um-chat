05af08c4eea34177625adcd9fcc1f9c00534638b
davewoo <daybreaker5@naver.com>
2020-05-19 14:15:25 +0900

    대폭수정!

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app = express();
+var redis = require('redis');
 var cookie = require('cookie-parser')
 var bodyParser = require('body-parser');
 var path = require('path');
 var http = require('http');
+var date = require('date-utils');
 var httpServer = http.createServer(app).listen(3080, function(req,res){
   console.log('Socket IO server has been started');
 });
 var io = require('socket.io').listen(httpServer);
-var cookieKey = 'aka.willshine_deV_um_@33aa~@#5';
+var secretKey = 'aka.willshine_deV_um_@33aa~@#5';
+var redisClient = redis.createClient();
 
+// var userList = {};
+// var redisKey = 'userlist';
 app.listen(3000, function(){
   var txt = '';
   if( process.env.NODE_ENV == 'production' ) {
@@ -19,16 +24,37 @@ app.listen(3000, function(){
     app.locals.pretty=true;
   }
   else{
-    txt = 'env undefine.. need set "export NODE_ENV=development"';
+    txt = 'env undefine.. need command "export NODE_ENV=development"';
   }
-  console.log('Connected 3000 port - ' + txt);
 });
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookie(cookieKey));
-app.set('view engine', 'jade');
-app.set('views', './views');
+app.use(cookie(secretKey));
+
+redisClient.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+function refreshUserList(roomId, user, push){
+  // console.log(roomId + ' / '+user.nickname + ' / '+user.socketId+' / '+ push);
+  if(push){
+    redisClient.hset(roomId, user.socketId, user.nickname, redis.print);
+  }else{
+    redisClient.hdel(roomId, user.socketId, redis.print);
+  }
+
+  redisClient.hgetall(roomId, function (err, userlist) {
+    // console.log(userlist);
+    if(userlist != undefined && userlist != null){
+      io.sockets.in(roomId).emit('room_userlist', userlist);
+    }
+  });
+
+}
 
 app.get('/', function(req, res){
   res.render('login');
@@ -36,15 +62,50 @@ app.get('/', function(req, res){
 
 app.post('/room', function(req, res){
   var nickname = req.body.nickname;
-  console.log(nickname);
   res.cookie('nickname', nickname);
-
   res.render('room', {nickname: nickname});
 });
 
+
+//status 001 - connect, 002 - disconnect
 io.on('connection', function(socket){
+  socket.on('join_room', function(data){
+    socket.join(data.roomId);
+    console.log(data.nickname+'['+socket.id+'] is join room :'+data.roomId);
+    var nickname = data.nickname;
+    var socketId = socket.id;
+    var datas = {
+      connection: true,
+      nickname: nickname
+    };
+    socket.username = nickname;
+    io.sockets.in(data.roomId).emit('room_arm', datas);
+    refreshUserList(data.roomId, {socketId: socketId, nickname: nickname}, true );
+  });
+
+  socket.on('leave_room', function(data){
+    socket.leave(data.roomId);
+    console.log(data.nickname+'['+socket.id+'] is leave room :'+data.roomId);
+    var nickname = data.nickname;
+    var socketId = socket.id;
+    var datas = {
+      connection: false,
+      nickname: nickname
+    };
+    io.sockets.in(data.roomId).emit('room_arm', datas);
+    refreshUserList(data.roomId, {socketId: socketId, nickname: nickname}, false);
+  });
+
   socket.on('chat_msg', function(data){
-    console.log('chat:'+data.nickname+':'+data.message);
-    io.emit('chat_msg', data);
+    var datetime = new Date().toFormat('YYYY-MM-DD HH24:MI:SS');
+    var datas = {
+      nickname: data.nickname,
+      message: data.message,
+      date: datetime
+    };
+    // socket.emit('chat_msg', datas);
+    io.sockets.in(data.roomId).emit('room_msg', datas);
+    // console.log('room:'+data.roomId
+    //             +' / chat:'+data.nickname+':'+data.message);
   });
 });
--- public/script/room.js
+++ public/script/room.js
@@ -1,4 +1,8 @@
-var server = 'localhost:3080';
+//var server = 'um-chat.tk:3080';
+var urlParser = document.createElement('a');
+urlParser.href = location.href;
+var server = urlParser.hostname+':3080';
+var blankPattern = /^\s+|\s+$/g;
 
 function getCookie(cname){
   var name = cname + "=";
@@ -12,23 +16,66 @@ function getCookie(cname){
 
 $(document).ready(function(){
   var socket = io.connect(server);
-  var nickname = getCookie('nickname');
+  var nickname = decodeURIComponent(getCookie('nickname'));
+  var roomId= '1818'
 
+  socket.emit('join_room',{roomId: roomId, nickname: nickname});
 
-  $(function () {
-    $('form').submit(function(){
-      var message = $('#mi').val();
-      socket.emit('chat_msg', {nickname : nickname, message : message});
-      $('#mi').val('');
-      return false;
-    });
-    socket.on('chat_msg', function(data){
-      $('#messages').append($('<li><span>'
-                                + data.nickname
-                                + '</span>:<span>'
-                                + data.message
-                                + '</span>'));
+  $(window).on('beforeunload', function(){
+    socket.emit('leave_room', {roomId: roomId, nickname: nickname});
+  });
+
+  socket.on('room_msg', function(data){
+    var msg = '';
+    if(data.nickname == nickname){
+      msg = '<li class="text-right"><span>'+ data.nickname
+          + '</span><span>' + data.date
+          + '</span><br><div>' + data.message
+          + '</div>'
+    }else{
+      msg = '<li><span>'+ data.nickname
+          + '</span><span>' + data.date
+          + '</span><br><div>' + data.message
+          + '</div>'
+    }
+    $('#messages').append($(msg));
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  });
+
+  socket.on('room_arm', function(data){
+    if(data.connection){
+      statusMsg = ' 님이 입장하셨습니다.</h4>';
+    }else{
+      statusMsg = ' 님이 퇴장하셨습니다.</h4>';
+    }
+    $('#messages').append($('<h4>' + data.nickname + statusMsg));
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  });
+
+  socket.on('room_userlist', function(userlist){
+    $('.users').remove();
+    $.each(userlist, function(key,value) {
+      //  console.log('key:'+key+', value:'+value);
+      $('#userlist').append('<span class="users" id="'+key+'">'+ value +'</span>');
     });
   });
 
-});
+
+  $('#chat_bar').submit(function(){
+    var message = $('#mi').val();
+    var isBlank = message.replace(blankPattern, '' ) == "" ? true : false;
+
+    if(!message || isBlank) return false;
+
+    var data =   {
+      roomId: roomId,
+      nickname : nickname,
+      message : message
+    };
+    socket.emit('chat_msg',data);
+    $('#mi').val('');
+    $('#mi').focus();
+    return false;
+  });
+
+}); //document.ready -- END
