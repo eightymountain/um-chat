99e75ee379c4a7cc976ee53ab646a5c7dc997582
davewoo <daybreaker5@naver.com>
2020-05-19 20:59:41 +0900

    대충수정함;

--- public/script/room.js
+++ public/script/room.js
@@ -1,81 +1,75 @@
 //var server = 'um-chat.tk:3080';
 var urlParser = document.createElement('a');
 urlParser.href = location.href;
-var server = urlParser.hostname+':3080';
+var server = urlParser.hostname;
 var blankPattern = /^\s+|\s+$/g;
 
-function getCookie(cname){
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0; i<ca.length; i++){
-    var c = ca[i].trim();
-    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-  }
-  return "";
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+    }
+    return "";
 }
 
-$(document).ready(function(){
-  var socket = io.connect(server);
-  var nickname = decodeURIComponent(getCookie('nickname'));
-  var roomId= '1818'
+$(document).ready(function () {
+    var socket = io.connect(server);
+    var nickname = decodeURIComponent(getCookie('nickname'));
+    var roomId = '1818'
 
-  socket.emit('join_room',{roomId: roomId, nickname: nickname});
+    var messages = $('#messages');
+    var mi = $('#mi');
+    var userlist = $('#userlist');
 
-  $(window).on('beforeunload', function(){
-    socket.emit('leave_room', {roomId: roomId, nickname: nickname});
-  });
+    socket.emit('join_room', {roomId: roomId, nickname: nickname});
 
-  socket.on('room_msg', function(data){
-    var msg = '';
-    if(data.nickname == nickname){
-      msg = '<li class="text-right"><span>'+ data.nickname
-          + '</span><span>' + data.date
-          + '</span><br><div>' + data.message
-          + '</div>'
-    }else{
-      msg = '<li><span>'+ data.nickname
-          + '</span><span>' + data.date
-          + '</span><br><div>' + data.message
-          + '</div>'
-    }
-    $('#messages').append($(msg));
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  });
+    socket.on('room_msg', function (data) {
+        var msg = '';
+        msg = '<li class="'
+        + (data.nickname === nickname) ? 'text-right' : ''
+            + '"><span>' + data.nickname
+            + '</span><span>' + data.date
+            + '</span><br><div>' + data.message
+            + '</div>'
 
-  socket.on('room_arm', function(data){
-    if(data.connection){
-      statusMsg = ' 님이 입장하셨습니다.</h4>';
-    }else{
-      statusMsg = ' 님이 퇴장하셨습니다.</h4>';
-    }
-    $('#messages').append($('<h4>' + data.nickname + statusMsg));
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  });
-
-  socket.on('room_userlist', function(userlist){
-    $('.users').remove();
-    $.each(userlist, function(key,value) {
-      //  console.log('key:'+key+', value:'+value);
-      $('#userlist').append('<span class="users" id="'+key+'">'+ value +'</span>');
+        messages.append($(msg));
+        messages.scrollTop(messages[0].scrollHeight);
     });
-  });
+
+    socket.on('room_arm', function (data) {
+        messages.append($('<h4>' + data.nickname + ' 님이 ' + data.connection ? '돌아왔다.' : '떠났다...'));
+        messages.scrollTop(messages[0].scrollHeight);
+    });
+
+    socket.on('room_userlist', function (list) {
+        $('.users').remove();
+        $.each(list, function (key, value) {
+            //  console.log('key:'+key+', value:'+value);
+            userlist.append('<span class="users" id="' + key + '">' + value + '</span>');
+        });
+    });
 
 
-  $('#chat_bar').submit(function(){
-    var message = $('#mi').val();
-    var isBlank = message.replace(blankPattern, '' ) == "" ? true : false;
+    $('#chat_bar').submit(function () {
+        var message = mi.val();
 
-    if(!message || isBlank) return false;
+        if (!message || message.replace(blankPattern, '') === "") return false;
 
-    var data =   {
-      roomId: roomId,
-      nickname : nickname,
-      message : message
-    };
-    socket.emit('chat_msg',data);
-    $('#mi').val('');
-    $('#mi').focus();
-    return false;
-  });
+        var data = {
+            roomId: roomId,
+            nickname: nickname,
+            message: message
+        };
+        socket.emit('chat_msg', data);
+        mi.val('');
+        mi.focus();
+        return false;
+    });
+
+    $(window).on('beforeunload', function () {
+        socket.emit('leave_room', {roomId: roomId, nickname: nickname});
+    });
 
 }); //document.ready -- END
