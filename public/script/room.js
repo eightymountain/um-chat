895d731c2793033bebcd9941835b308cb2bf6ca9
davewoo <daybreaker5@naver.com>
2020-05-28 13:25:51 +0900

    reformat code, 액션메세지 기능추가, layout 수정

--- public/script/room.js
+++ public/script/room.js
@@ -1,33 +1,29 @@
-//var server = 'um-chat.tk:3080';
-var urlParser = document.createElement('a');
-urlParser.href = location.href;
-var server = urlParser.hostname;
+var server = location.hostname;
 var blankPattern = /^\s+|\s+$/g;
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-    }
-    return "";
-}
+var toolbox;
+var userlist;
+var messages;
+var mi;
+var socket;
+var nickname;
+var roomId
+var toolboxTarget  = undefined;
 
 $(document).ready(function () {
-    var socket = io.connect(server);
-    var nickname = decodeURIComponent(getCookie('nickname'));
-    var roomId = '1818'
-
-    var messages = $('#messages');
-    var mi = $('#mi');
-    var userlist = $('#userlist');
+    socket = io.connect(server);
+    nickname = decodeURIComponent(getCookie('nickname'));
+    roomId = decodeURIComponent(getCookie('roomId'));
+    mi = $('#mi');
+    messages = $('#messages');
+    userlist = $('#userlist');
+    toolbox = $('#toolbox');
 
     socket.emit('join_room', {roomId: roomId, nickname: nickname});
 
     socket.on('room_msg', function (data) {
         var msg = '<li class="'
-        + (data.nickname === nickname ? 'text-right' : '')
+            + (data.nickname === nickname ? 'text-right' : '')
             + '"><span>' + data.nickname
             + '</span><span>' + data.date
             + '</span><br><div>' + data.message
@@ -38,7 +34,7 @@ $(document).ready(function () {
     });
 
     socket.on('room_arm', function (data) {
-        var msg = '<h4>' + data.nickname + ' 님이 ' + (data.connection ? '돌아왔다.' : '떠났다...') +'</h4>';
+        var msg = '<h4>' + data.nickname + ' 님이 ' + (data.connection ? '돌아왔다.' : '떠났다...') + '</h4>';
         messages.append($(msg));
         messages.scrollTop(messages[0].scrollHeight);
     });
@@ -46,26 +42,26 @@ $(document).ready(function () {
     socket.on('room_userlist', function (list) {
         $('.users').remove();
         $.each(list, function (key, value) {
-            //  console.log('key:'+key+', value:'+value);
-            userlist.append('<span class="users" id="' + key + '">' + value + '</span>');
+            userlist.append('<div class="users" id="' + key + '"><span class="nick">' + value + '</span></div>');
         });
     });
 
 
     $('#chat_bar').submit(function () {
         var message = mi.val();
-
         if (!message || message.replace(blankPattern, '') === "") return false;
 
-        var data = {
-            roomId: roomId,
-            nickname: nickname,
-            message: message
-        };
-        socket.emit('chat_msg', data);
+        sendMsg(message);
+
         mi.val('');
         mi.focus();
+
         return false;
+    });
+
+    $(document).on('click', "#userlist .users", function () {
+        toolbox.detach().appendTo(this);
+        toolbox.show();
     });
 
     $(window).on('beforeunload', function () {
@@ -73,3 +69,34 @@ $(document).ready(function () {
     });
 
 }); //document.ready -- END
+
+function toolboxAction(ele, msg, e) {
+    var nm = $('#toolbox').parent().children()[0].innerText
+    sendMsg(nickname + '님이 ' + nm+' 님을 ' + msg);
+}
+
+function closeToolbox(e) {
+    e.stopPropagation();
+    if (toolbox) {
+        toolbox.hide();
+    }
+    return false;
+}
+
+function sendMsg(msg) {
+    socket.emit('chat_msg', {
+        roomId: roomId,
+        nickname: nickname,
+        message: msg
+    });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
